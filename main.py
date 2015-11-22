9e58815b69d1df88c2893bec4ddb482e5294f562
loono <linnanthu@163.com>
2015-11-22 18:35:19 -0500

    done

--- main.py
+++ main.py
@@ -1,16 +1,97 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
+#coding=utf-8
+
 from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+import md5
+import datetime
+from google.appengine.ext import db
+from jinja2 import Markup
+
+
+class Article(db.Model):
+    name = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    fileid = db.StringProperty(required=True)
+    time = db.StringProperty(required=True)
+
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def index():
+
+    articles = db.GqlQuery("SELECT * FROM Article ORDER BY time")
+    
+    files = []
+    for f in articles:
+        a = {}
+        a['title'] = f.name
+        a['content'] = Markup(f.content.replace(' ', '<br>'))
+        files.append(a)
+    
+
+    files.reverse()
+
+    return render_template('index.html', files=files)
+
+@app.route('/update')
+def add_content():
+    return render_template('update.html')
+
+@app.route('/post', methods=['POST'])
+def post_content():
+    
+    if md5.new(request.form['code']).hexdigest() != '':
+    	return redirect(url_for('add_content'))
+
+    title = request.form['title']
+    time = str(datetime.datetime.now())
+    fileid = md5.new(time).hexdigest()
+
+    content = request.form['content']
+
+    f = Article(name=title,
+    			time=time,
+    			fileid=fileid,
+    			content=content)
+
+    f.put()
+
+    return redirect(url_for('add_content'))
+
+
+@app.route('/delete')
+def delete():
+
+    articles = db.GqlQuery("SELECT * FROM Article")
+
+    files = []
+    for f in articles:
+        a = {}
+        a['title'] = f.name
+        a['fileid'] = f.fileid
+        files.append(a)
+    
+    return render_template('delete.html', files=files)
+
+
+@app.route('/remove', methods=['POST'])
+def remove():
+    
+    for f in request.form.getlist('files'):
+    	f = "'" + f + "'"
+        q = db.GqlQuery("SELECT * FROM Article WHERE fileid = " + f)
+        db.delete(q.fetch(10))
+    
+    return redirect(url_for('delete'))
+
+
 
 
 @app.errorhandler(404)
@@ -23,3 +104,6 @@ def page_not_found(e):
 def application_error(e):
     """Return a custom 500 error."""
     return 'Sorry, unexpected error: {}'.format(e), 500
+
+if __name__ == '__main__':
+	app.run(debug=True)
